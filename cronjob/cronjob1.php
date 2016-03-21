7c9d5ffb2592e7f4393398b041081d767d4ba230
dilpreet1 <dilpreet.sekhon25@gmail.com>
2016-03-21 12:03:14 +0530

    Update cronjob1.php

--- cronjob/cronjob1.php
+++ cronjob/cronjob1.php
@@ -1,7 +1,7 @@
 #!/bin/sh
 <?php
 $to      = 'dilpreet@ourdesignz.com';
-$subject = 'testing';
+$subject = 'online git testing';
 $message = 'hello';
 $headers = 'From: testing#gmail.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
